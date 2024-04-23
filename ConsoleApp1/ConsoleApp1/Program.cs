33eed1fb8144a58de76c28ffe6b5711daf1a32e2
zuzkaluzka <166519571+zuzkaluzka@users.noreply.github.com>
2024-04-23 13:56:53 +0200

    tworzenie_tab

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,14 @@
-﻿class Program
+﻿using ConsoleApp1;
+
+class Program
 {
     static void Main(string[] args)
     {
+        Tablice tablice = new Tablice();
+        tablice.Powitanie();
+
+        int[] tab = tablice.StworzTablice();
+
 
     }
 }
--- ConsoleApp1/ConsoleApp1/tablice.cs
+++ ConsoleApp1/ConsoleApp1/tablice.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Tablice
+
+namespace ConsoleApp1
 {
-    public void Powitanie()
+    public class Tablice
     {
-        Console.WriteLine();
+        public int liczba = 20;
+        public void Powitanie()
+        {
+            Console.WriteLine("Witaj w pliku tablice!");
+        }
+        public int[] StworzTablice()
+        {
+            Console.Write("Podaj długość tablicy: ");
+            int dlugosc = int.Parse(Console.ReadLine());
+            int[] tabl = new int[dlugosc];
+            for (int i = 0; i < tabl.Length; i++)
+            {
+                Console.Write("Podaj kolejny element tablicy: ");
+                tabl[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            return tabl;
+        }
     }
 }
